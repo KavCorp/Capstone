661379072830f4b0bba785fef063c738fa1b43ec
CodeUh <CodeUh@gmail.com>
2015-10-12 13:14:23 -0500

    Added CategoryService and CategoryServiceImpl with basic functions copied from the UserServices

--- Capstone/src/main/java/com/araxsys/repositories/UserRepository.java
+++ Capstone/src/main/java/com/araxsys/repositories/UserRepository.java
@@ -1,7 +1,6 @@
 package com.araxsys.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
 import com.araxsys.domain.User;
 
 public interface UserRepository extends CrudRepository<User, String>{
