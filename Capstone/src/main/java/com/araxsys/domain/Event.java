8e79bc51a37201f69f964c0db4dc79f7941e16b7
kamon42 <kamon42@yahoo.com>
2015-10-26 09:04:13 -0500

    Continuing Calender stuffz

--- Capstone/src/main/java/com/araxsys/domain/Event.java
+++ Capstone/src/main/java/com/araxsys/domain/Event.java
@@ -1,5 +1,23 @@
 package com.araxsys.domain;
 
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 public class Event {
+	
+	
 
 }
