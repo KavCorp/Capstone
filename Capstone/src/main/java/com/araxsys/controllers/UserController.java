1018f815118c62069f71816bb52488d5fb0712a9
cisco <cisco@BD145B12.csad.uafs.edu>
2015-11-23 13:53:34 -0600

    Security dialect/logout

--- Capstone/src/main/java/com/araxsys/controllers/UserController.java
+++ Capstone/src/main/java/com/araxsys/controllers/UserController.java
@@ -1,6 +1,12 @@
 package com.araxsys.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -73,12 +79,21 @@ public class UserController {
 
 		if (logout != null) {
 			model.addAttribute("msg", "You've been logged out successfully.");
+			
 		}
 
 
 		return "login";
 
 	}
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){    
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
+    }
     
     
     
